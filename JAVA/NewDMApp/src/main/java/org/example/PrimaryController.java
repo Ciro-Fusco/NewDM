ab11f4693349b2b8658ac516b979b81513c006d6
YoungHidden <vincenzoaiello300@gmail.com>
2020-12-21 10:55:29 +0100

    NewDMApp JavaFX

---
+++ JAVA/NewDMApp/src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        System.out.println("ciao");
+    }
+}
