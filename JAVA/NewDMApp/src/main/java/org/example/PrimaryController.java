2a436d3f366305462be345907eb1e1340d77f437
Ciro <cirof999@gmail.com>
2020-12-21 23:01:06 +0100

    Divertimento delle 11 di sera

--- JAVA/NewDMApp/src/main/java/org/example/PrimaryController.java
+++ JAVA/NewDMApp/src/main/java/org/example/PrimaryController.java
@@ -4,11 +4,26 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
+
+    
+    public TextField us;
+    public PasswordField pass;
 
     @FXML
     private void switchToSecondary() throws IOException {
         System.out.println("ciao");
+    }
+
+
+    public void checkLogin(MouseEvent mouseEvent) throws IOException {
+
+        System.out.println(us.getText()+" "+pass.getText());
+        App.setRoot("Dashboard");
+
     }
 }
